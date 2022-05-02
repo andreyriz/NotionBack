ce24388df67fe54fcffde8ab68b8a6f5d0ace386
a.riazantsev <andreyriz2909@gmail.com>
2022-05-02 15:47:48 +0300

    11

--- src/tables/Musics.kt
+++ src/tables/Musics.kt
@@ -6,8 +6,8 @@ import org.jetbrains.exposed.sql.Table
 object Musics : Table("music") {
     val music_id: Column<Int> = integer("music_id").autoIncrement()
     val music_name: Column<String> = varchar("music_name", 100)
-    val music_img: Column<String> = varchar("music_img", 100)
-    val music_source: Column<String> = varchar("music_source", 100)
+    val music_img: Column<String> = varchar("banner", 100)
+    val music_source: Column<String> = varchar("source", 100)
     val year: Column<Int> = integer("year_of_issue")
     override val primaryKey = PrimaryKey(music_id)
 }
