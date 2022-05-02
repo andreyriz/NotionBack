324df7e05951cd42591d78a7aaeb2779be4bb039
a.riazantsev <andreyriz2909@gmail.com>
2022-05-02 15:46:43 +0300

    11

--- src/tables/Musics.kt
+++ src/tables/Musics.kt
@@ -3,7 +3,7 @@ package com.musiclibrarysusie.tables
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
-object Musics : Table("Music") {
+object Musics : Table("music") {
     val music_id: Column<Int> = integer("music_id").autoIncrement()
     val music_name: Column<String> = varchar("music_name", 100)
     val music_img: Column<String> = varchar("music_img", 100)
